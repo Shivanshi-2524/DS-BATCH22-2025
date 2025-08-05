bdb602d0402a8ee22641ee77c97cbd46a45ee19e
visha <shrutikanade296@gmail.com>
2025-08-05 21:54:54 +0530

    sample

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("my name is Shruti")
     print(this is github)
+    print(hii)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
