45f079e48698fe7e5789181a8e28e46fe37d89f7
visha <shrutikanade296@gmail.com>
2025-08-05 22:06:57 +0530

    started developer branch

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("my name is Shruti")
+    print("this is developer branch started")
     print(this is github)
     print(hii)
 
